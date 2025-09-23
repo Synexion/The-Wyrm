3c7216e906ff222da09d0b6ddd4a7f6870762d17
Casagrande Lilian <liliansynexion@gmail.com>
2025-09-23 17:51:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Bienvenue au sein de la confrérie des protecteurs ! Rejoignez la bataille pour sauver Yehenor !">
+  <link rel="stylesheet" href="./style/style.css">
+  <link rel="shortcut icon" href="./assets/logo.png" type="image/x-icon">
+  <title>The Wyrm</title>
+</head>
+<body>
+  <!-- barre de navigation -->
+  <nav class="nav_bar">
+    <a href="#">Accueil</a>
+    <a href="#">Jouer maintenant !</a>
+    <a href="#">Actualités</a>
+    <a href="#">Forum</a>
+    <a href="#">Contact</a>
+    <a href="#">S'inscrire / Se connecter</a>
+  </nav>
+  <header class="header_content">
+    <div class="header_shadow"></div>
+    <div class="header_text">
+      <h1>The Wyrm</h1>
+      <p>Rejoignez les rangs. La défaite ne sera pas permise.</p>
+      <div class="header_cta">
+        <a href="#">jouer maintenant</a>
+      </div>
+    </div>
+  </header>
+  <main class="main_content">
+    <div class="main_nav">
+      <div class="quete">
+        <a href="#"><h3>Sauvez notre monde</h3></a>
+      </div>
+      <div class="hero">
+        <a href="#"><h3>Définissez qui vous êtes</h3></a>
+      </div>
+      <div class="exploration">
+        <a href="#"><h3>Découvrez un monde magique</h3></a>
+      </div>
+    </div>
+    <div class="main_presentation">
+      <div class="main_img">
+        <img src="./assets/main_img.jpg" alt="chevalier combattant des ennemies">
+      </div>
+      <div class="main_text">
+        <div class="main_text">
+          <p>Bienvenue dans The Wyrm.</p>
+          <p>Arpenter un monde créer par un unique développeur passionné des mondes d'héroïque fantaisie.</p>
+          <p>Ce jeu à pour but de me perfectionner dans le domaine du développement. Aussi bien vers le développement web par le biais de ce site internet et du développement du jeu vidéo The Wyrm.</p>
+          <p>Ce site me servira de Portfolio, dans l'objectif de trouver une alternance afin de parfaire mes aptitudes dans ces domaines.</p>
+          <p>Tout retour constructif est évidemment bienvenue. Vous pouvez les transmettres grâce au formulaire de contact situé dans la vue contact, qui est accescible par la barre de navigation.</p>
+        </div>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <div class="footer_logo">
+      <img src="./assets/logo-ssf.png" alt="logo the wyrm sans fond">
+    </div>
+    <div class="footer_links">
+      <div class="footer_link">
+        <div class="footer_link_title">
+          <h2>Liens</h2>
+        </div>
+        <div class="links">
+          <a href="#">Accueil</a>
+          <a href="#">Jouer maintenant !</a>
+          <a href="#">Actualités</a>
+          <a href="#">Forum</a>
+          <a href="#">Contact</a>
+          <a href="#">S'inscrire / Se connecter</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="./js/js.js"></script>
+</body>
+</html>
